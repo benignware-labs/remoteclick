30a3142a5ce6045037fe88951279c2d2606c6b22
Rafael Nowrotek <mailbenignware.com>
2019-03-20 00:28:24 +0100

    Pass function for options and integrate pending state

--- src/remoteclick.js
+++ src/remoteclick.js
@@ -1,107 +1,121 @@
 import uniqueSelector from 'unique-selector';
 
 function remoteclick(selector, options = {}) {
-  options = {
-    srcAttr: 'href',
-    mode: 'append',
-    history: false,
-    containerSelector: null,
-    ...options
-  };
 
   const handleClick = (event) => {
     const target = event.target.closest(selector);
 
     if (target) {
-      if (target.matches(selector)) {
-        const container = options.containerSelector
-          ? document.querySelector(options.containerSelector)
-          : target.parentNode;
-        const href = target.getAttribute(options.srcAttr) || target.getAttribute(`data-${options.srcAttr}`);
+      // Get options
+      options = {
+        srcAttr: 'href',
+        mode: 'append',
+        history: false,
+        containerSelector: null,
+        pendingClass: 'is-pending',
+        ...(
+          typeof options === 'function' ? options(target) : options
+        )
+      };
 
-        if (container && href) {
-          const url = new URL(href, window.location.href);
-          const containerSelector = options.containerSelector || uniqueSelector(container);
-          const targetSelector = uniqueSelector(target);
+      const container = options.containerSelector
+        ? document.querySelector(options.containerSelector)
+        : target.parentNode;
+      const href = target.getAttribute(options.srcAttr) || target.getAttribute(`data-${options.srcAttr}`);
 
-          fetch(url.href, {
-            // Request options
-          }).then(response => {
-            response.text().then(html => {
-              // Parse html
-              const dom = document.createElement( 'div' );
-              dom.innerHTML = html;
+      if (container && href) {
+        const url = new URL(href, window.location.href);
+        const containerSelector = options.containerSelector || uniqueSelector(container);
+        const targetSelector = uniqueSelector(target);
 
-              // Find remote target
-              const remoteTarget = dom.querySelector(targetSelector);
+        // Add pending class
+        options.pendingClass.split(/\s+/).forEach((className) => {
+          target.classList.add(className);
+        });
 
-              if (remoteTarget) {
-                // Remove attributes
-                for (let { name } of dom.attributes) {
-                  target.removeAttribute(name);
-                }
-
-                // Update attributes
-                for (let { name, value } of remoteTarget.attributes) {
-                  target.setAttribute(name, value);
-                }
-
-                // Update link content
-                target.innerHTML = remoteTarget.innerHTML;
-              } else {
-                // End of list
-                target.parentNode.removeChild(target);
-              }
-
-              // Find remote container
-              const remoteContainer = dom.querySelector(containerSelector);
-
-              if (remoteContainer) {
-                // Create content fragment
-                const fragment = document.createDocumentFragment();
-                // Take care of link wrappers
-                const remoteTargetWrapper = [ ...remoteContainer.childNodes ]
-                  .find((child) => child.contains(remoteTarget)) || remoteTarget;
-                const targetWrapper = [ ...container.childNodes ]
-                  .find((child) => child.contains(target)) || target;
-
-                for (const child of remoteContainer.children) {
-                  fragment.appendChild(
-                    child === remoteTargetWrapper
-                      ? targetWrapper
-                      : document.importNode(child, true)
-                  );
-                }
-
-                // Actually update dom content
-                switch (options.mode) {
-                case 'update':
-                  container.appendChild(fragment);
-                  break;
-                case 'prepend':
-                  container.insertBefore(container.firstChild, fragment);
-                  break;
-                case 'append':
-                  container.appendChild(fragment);
-                  break;
-                }
-
-              }
-
-              // History
-              if (options.history) {
-                // Find remote title
-                const title = (dom.querySelector(targetSelector) || {
-                  title: document.title
-                }).title;
-
-                history.pushState({
-                  title
-                }, title, url.href);
-              }
+        // Fetch remote content
+        fetch(url.href, {
+          // Request options
+        }).then(response => {
+          response.text().then(html => {
+            // Remove pending class
+            options.pendingClass.split(/\s+/).forEach((className) => {
+              target.classList.add(className);
             });
+
+            // Parse html
+            const dom = document.createElement( 'div' );
+            dom.innerHTML = html;
+
+            // Find remote target
+            const remoteTarget = dom.querySelector(targetSelector);
+
+            if (remoteTarget) {
+              // Remove attributes
+              for (let { name } of dom.attributes) {
+                target.removeAttribute(name);
+              }
+
+              // Update attributes
+              for (let { name, value } of remoteTarget.attributes) {
+                target.setAttribute(name, value);
+              }
+
+              // Update link content
+              target.innerHTML = remoteTarget.innerHTML;
+            } else {
+              // End of list
+              target.parentNode.removeChild(target);
+            }
+
+            // Find remote container
+            const remoteContainer = dom.querySelector(containerSelector);
+
+            if (remoteContainer) {
+              // Create content fragment
+              const fragment = document.createDocumentFragment();
+              // Take care of link wrappers
+              const remoteTargetWrapper = [ ...remoteContainer.childNodes ]
+                .find((child) => child.contains(remoteTarget)) || remoteTarget;
+              const targetWrapper = [ ...container.childNodes ]
+                .find((child) => child.contains(target)) || target;
+
+              for (const child of remoteContainer.children) {
+                fragment.appendChild(
+                  child === remoteTargetWrapper
+                    ? targetWrapper
+                    : document.importNode(child, true)
+                );
+              }
+
+              // Actually update dom content
+              switch (options.mode) {
+              case 'update':
+                container.appendChild(fragment);
+                break;
+              case 'prepend':
+                container.insertBefore(container.firstChild, fragment);
+                break;
+              case 'append':
+                container.appendChild(fragment);
+                break;
+              }
+
+            }
+
+            // History
+            if (options.history) {
+              // Find remote title
+              const title = (dom.querySelector(targetSelector) || {
+                title: document.title
+              }).title;
+
+              history.pushState({
+                title
+              }, title, url.href);
+            }
           });
-        }
+        });
       }
 
       event.preventDefault();
---
+++ test/fixtures/script.js
@@ -0,0 +1,4 @@
+window.remoteclick('*[data-remote]', () => ({
+  pendingClass: 'pending',
+  containerSelector: '#content'
+}));
